e5e917e8916059822c1d0c2e6f7d22b7ed1e2776
Fabrice Brito <fabrice.brito@terradue.com>
2014-06-03 22:08:10 +0200

    Update IsISOCodeInvalid.R

--- src/main/R/Reowb-cckp/R/IsISOCodeInvalid.R
+++ src/main/R/Reowb-cckp/R/IsISOCodeInvalid.R
@@ -12,15 +12,15 @@
 
 IsISOCodeInvalid <- function(ISO.code){
 codes <- c(NoAm_country,SoAm_country,Oceana_country,Africa_country,Asia_country,Eur_country)
-  if (nchar(iso) != 3 && is.character(ISO.code)) { 
+  if (nchar(ISO.code) != 3 && is.character(ISO.code)) { 
     print("Please enter a valid 3 letter country code")
     return(TRUE)
   }
-  if (is.numeric(iso)) {
+  if (is.numeric(ISO.code)) {
     print("Please enter a 3 letter code, not a number")
     return(TRUE)  
   }
-  if (!toupper(iso)%in%codes) {
+  if (!toupper(ISO.code)%in%codes) {
     print(paste(ISO.code,"is an invalid 3 letter country code, please refer to http://unstats.un.org/unsd/methods/m49/m49alpha.htm for a valid list",sep=" "))
     return(TRUE)
   }
