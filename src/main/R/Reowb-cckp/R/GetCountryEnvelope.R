f4218cdf3479e72260a89118d02b8e95fe7dd9c3
Fabrice Brito <fabrice.brito@terradue.com>
2014-06-03 22:04:27 +0200

    Update GetCountryEnvelope.R

--- src/main/R/Reowb-cckp/R/GetCountryEnvelope.R
+++ src/main/R/Reowb-cckp/R/GetCountryEnvelope.R
@@ -1,6 +1,10 @@
 
 GetCountryEnvelope <- function(ISO.Code) {
 
+  if (IsISOCodeInvalid) { stop(paste(ISO.Code, "is not valid", sep=" ")) }
   
+  bbox <- gEnvelope(world_EEZ_V8_2014[world_EEZ_V8_2014$ISO_3digit == "PRT",])@bbox
 
+  print(paste(bbox[1,1], bbox[2,1], bbox[1,2], bbox[2,2], sep=","))
+  
 }
