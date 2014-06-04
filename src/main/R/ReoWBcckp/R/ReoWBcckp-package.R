65979e671e6a827c702e71028e5ad7687a8eb3dc
Fabrice Brito <fabrice.brito@terradue.com>
2014-06-04 07:19:52 +0200

    Create ReoWBcckp-package.R

---
+++ src/main/R/ReoWBcckp/R/ReoWBcckp-package.R
@@ -0,0 +1,77 @@
+#' isocodes data  
+#' @name codes
+#' @docType data
+#' @keywords datasets
+NULL
+
+#' Basin codes for Africa
+#' @name Africa_basin
+#' @docType data
+#' @keywords datasets
+NULL
+
+#' Country codes for all of Africa
+#' @name Africa_country
+#' @docType data
+#' @keywords datasets
+NULL
+
+#' Basin codes for Asia
+#' @name Asia_basin
+#' @docType data
+#' @keywords datasets
+NULL
+
+#' Country codes for all of Asia
+#' @name Asia_country
+#' @docType data
+#' @keywords datasets
+NULL
+
+#' Basin codes for Eur
+#' @name Eur_basin
+#' @docType data
+#' @keywords datasets
+NULL
+
+#' Country codes for all of Eur
+#' @name Eur_country
+#' @docType data
+#' @keywords datasets
+NULL
+
+#' Basin codes for NoAm, used in downloading maps
+#' @name NoAm_basin
+#' @docType data
+#' @keywords datasets
+NULL
+
+#' Country codes for all of NoAm
+#' @name NoAm_country
+#' @docType data
+#' @keywords datasets
+NULL
+
+#' Basin codes for Oceana
+#' @name Oceana_basin
+#' @docType data
+#' @keywords datasets
+NULL
+
+#' Country codes for all of Oceana
+#' @name Oceana_country
+#' @docType data
+#' @keywords datasets
+NULL
+
+#' Basin codes for SoAm
+#' @name SoAm_basin
+#' @docType data
+#' @keywords datasets
+NULL
+
+#' Country codes for all of SoAm
+#' @name SoAm_country
+#' @docType data
+#' @keywords datasets
+NULL
