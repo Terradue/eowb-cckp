30048d15e05877d898151fbebddaa71bfdb5b78e
Fabrice Brito <fabrice.brito@terradue.com>
2014-06-04 08:26:07 +0200

    Create getCountryEEZ.R

---
+++ src/main/R/ReoWBcckp/R/getCountryEEZ.R
@@ -0,0 +1,19 @@
+#'provides the country EEZ spatial polygon(s)
+#'@description Checks if the country code entered is valid 
+#' and provides the country exclusive economic zone (EEZ) spatial polygon(s) 
+#'
+#'@param ISO.code The 3 letter country code based on ISO3 Country abbreviations (http://unstats.un.org/unsd/methods/m49/m49alpha.htm)
+#'@return SpatialPolygons
+#'@examples \dontrun{
+#'GetCountryEEZ("PRT")
+#'}
+#'
+#' @export
+
+GetCountryEEZ <- function(ISO.Code) {
+
+  if (IsISOCodeInvalid(ISO.Code)) { stop(paste(ISO.Code, "is not valid", sep=" ")) }
+  
+  return(SpatialPolygons(world_EEZ_V8_2014[world_EEZ_V8_2014$ISO_3digit == toupper(ISO.Code),]@polygons))
+  
+}
