296250da31751f9622397b14ac8d0cc30ffc157d
Fabrice Brito <fabrice.brito@terradue.com>
2014-06-04 07:29:45 +0200

    Update GetCountryEnvelope.R

--- src/main/R/ReoWBcckp/R/GetCountryEnvelope.R
+++ src/main/R/ReoWBcckp/R/GetCountryEnvelope.R
@@ -1,3 +1,13 @@
+#'provides the country EEZ envelope
+#'@description Checks if the country code entered is valid 
+#' and provides the geographical envelope of the country exclusive economic zone (EEZ) 
+#'
+#'@param iso The 3 letter country code based on ISO3 Country abbreviations (http://unstats.un.org/unsd/methods/m49/m49alpha.htm)
+#'@return character representation of the geographical envelope as xmin, ymin, xmax, ymax
+#'@examples \dontrun{
+#'GetCountryEnvelope("PRT")
+#'}
+#'
 
 GetCountryEnvelope <- function(ISO.Code) {
 
