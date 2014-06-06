298370f889d4d8ea3ed637f8bd59732b23beebe6
Fabrice Brito <fabrice.brito@terradue.com>
2014-06-06 10:59:30 +0200

    Update run.R

--- src/main/app-resources/wb-sea-level/run.R
+++ src/main/app-resources/wb-sea-level/run.R
@@ -1,1 +1,17 @@
-Print("Hello R!")
+#!/usr/bin/Rscript --vanilla --slave --quiet
+ 
+library("rciop")
+library("rgeos")
+
+rciop.log("DEBUG", paste("track", os.track, "start", os.start, "stop", os.stop, sep=" "))
+
+# read the stdin into a file
+f <- file("stdin")
+open(f)
+
+while(length(input <- readLines(f, n=1)) > 0) {
+  
+  rciop.log("DEBUG", paste("Country ISO code:", input, sep=" "))
+  
+}
+  
