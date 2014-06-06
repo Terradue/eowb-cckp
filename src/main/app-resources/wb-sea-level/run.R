ca14f0bfb54254eaf00dc5f66152437b44ae8e5e
Fabrice Brito <fabrice.brito@terradue.com>
2014-06-06 11:13:53 +0200

    Update run.R

--- src/main/app-resources/wb-sea-level/run.R
+++ src/main/app-resources/wb-sea-level/run.R
@@ -3,15 +3,83 @@
 library("rciop")
 library("rgeos")
 
+osd.url <- rciop.getparam("catalogue")
+start.date <- rciop.getparam("start.date")
+end.date <- rciop.getparam("end.date")
+response.type <- rciop.getparam("response.type")
+count <- rciop.getparam(count)
+
 rciop.log("DEBUG", paste("track", os.track, "start", os.start, "stop", os.stop, sep=" "))
+
+# prepare the catalogue request
+df.params <- GetOSQueryables(osd.url, response.type)
+
+df.params$value[df.params$type == "count"] <- count 
+df.params$value[df.params$type == "time:start"] <- start.date
+df.params$value[df.params$type == "time:end"] <- end.date 
+
+# submit the query
+res <- Query(osd.url, response.type, df.params)
+
+# create a named list with the WCS online resources and associated start date 
+coverages <- list(online.resource=rev(xpathSApply(xmlParse(res), "//dclite4g:DataSet/dclite4g:onlineResource/ws:WCS/@rdf:about")), 
+      start=rev(xpathSApply(xmlParse(res), "//dclite4g:DataSet/ical:dtstart", xmlValue)))
+
+# prepare the WCS request
+wcs.template <- GetWCSTemplate()
+
+wcs.template$value[wcs.template$param == "service"] <- "WCS" 
+wcs.template$value[wcs.template$param == "version"] <- "1.0.0"
+wcs.template$value[wcs.template$param == "request"] <- "GetCoverage"
+wcs.template$value[wcs.template$param == "coverage"] <- "sla"
+wcs.template$value[wcs.template$param == "format"] <- "NetCDF3"
 
 # read the stdin into a file
 f <- file("stdin")
 open(f)
 
-while(length(input <- readLines(f, n=1)) > 0) {
+while(length(country.code <- readLines(f, n=1)) > 0) {
   
-  rciop.log("DEBUG", paste("Country ISO code:", input, sep=" "))
+  rciop.log("DEBUG", paste("Country ISO code:", country.code, sep=" "))
   
+  # complete the WCS request with the country envelope (MBR) 
+  wcs.template$value[wcs.template$param == "bbox"] <- GetCountryEnvelope(country.code)
+  
+  r.stack <- c()
+  idx <- c()
+
+  for (i in 1:length(coverages$online.resource)) {
+  
+    rciop.log("INFO", paste(i/length(coverages$online.resource)*100, "Processing date:",  format(as.Date(coverages$start[i]), format="%Y-%m"), sep=" "))
+  
+    # get the coverage 
+    r <- GetWCSCoverage(coverages$online.resource[i], wcs.template, by.ref=FALSE)
+    
+    # issue on georef for contries with longitudes<0
+    r.shift <- shift(r, x=-360,y=0)
+    
+    # clip with the country EEZ
+    r.mask <- mask(r.shift, GetCountryEEZ(country.code))
+    
+    # add the clipped raster to the list
+    r.stack <- c(r.stack, r.mask)
+    
+    # update the index
+    idx <- c(idx, format(as.Date(coverages$start[i]), format="%Y-%m"))
+    
+    # delete the WCS downloaded raster (the other raster are in memory)
+    file.remove(r@file@name)
+  
+  }
 }
   
+# create the stack
+my.stack <- setZ(stack(r.stack), idx)
+names(my.stack) <- idx
+
+result.filename <- paste(TMPDIR, "/", country.code, ".list")
+save(cellStats(my.stack, 'mean'), file=filename)
+
+res <- rciop.publish(result.filename, FALSE, FALSE)
+ 
+if (res$exit.code==0) { published <- res$output }
