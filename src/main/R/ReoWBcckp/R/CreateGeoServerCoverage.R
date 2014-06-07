b3ea3575b885d7cf2c68c7f6d053db5009b57bdd
Fabrice Brito <fabrice.brito@terradue.com>
2014-06-07 08:29:44 +0200

    Update CreateGeoServerCoverage.R

--- src/main/R/ReoWBcckp/R/CreateGeoServerCoverage.R
+++ src/main/R/ReoWBcckp/R/CreateGeoServerCoverage.R
@@ -2,14 +2,17 @@
 #' @description Creates a GeoServer coverage store in a GeoServer instance.
 #'
 #' @param geoserver.access.point GeoServer REST API access point
-#' @param workspace character vector of the GeoServer workspace
+#' @param workspace character vector of the GeoServer workspace (must exist)
+#' @param coveragestore character vector of the GeoServer coveragestore (must exist)
 #' @param name character vector with the coverage store name
+#' @param title character vector with the coverage store title
+#' @param abstract character vector with the coverage store abstract
 #' @param enabled boolean to define if the coverage store is enabled, defaults to TRUE
-#' @param type character vector with the coverage type, defaults to GeoTIFF
-#' @param url URL of a coverage previously uploaded
-#' @return boolean TRUE when the coverage store was created, FALSE otherwise (check message returned)
+#' @param native.crs native CRS of the coverage (defaults to EPSG:4326)
+#' @param latlon.crs Lat/Lon CRS of the coverage (defaults to EPSG:4326)
+#' @return boolean TRUE when the coverage was created, FALSE otherwise (check message returned)
 #' @examples \dontrun{
-#' CreateGeoServerCoverageStore("localhost:8080/geoserver", "acme", "cs", "file:data/folder/coverage")
+#' CreateGeoServerCoverage("localhost:8080/geoserver", "acme", "cs", "mycoverage", "this is a title")
 #' }
 #'
 #' @export
@@ -17,7 +20,7 @@
 
 CreateGeoServerCoverageStore <- function(geoserver.access.point, 
     workspace, 
-    coverage.store, 
+    coveragestore, 
     name, 
     title, 
     abstract="This is the abstract",
