bdb7f56ccc6b9f935540a4545b38824aa6a4bb87
Fabrice Brito <fabrice.brito@terradue.com>
2014-06-07 16:20:06 +0200

    Create POSTRequest.R

---
+++ src/main/R/ReoWBcckp/R/POSTRequest.R
@@ -0,0 +1,23 @@
+
+POSTRequest <- function(access.point, content.type, content) {
+  
+  myheader=c(Connection='close', 'Content-Type' = content.type)
+  
+  reader = basicTextGatherer()
+  header = basicTextGatherer()
+  
+  data <- curlPerform(url = access.point,
+    postfields = content,
+    userpwd = "admin:geoserver",
+    httpheader = myheader,
+    verbose = FALSE,
+    ssl.verifypeer = FALSE,
+    writefunction = reader$update,
+    headerfunction = header$update
+  )
+  
+  h = parseHTTPHeader( header$value() )
+  
+  return(list(h["status"], h["statusMessage"]))
+  
+}
