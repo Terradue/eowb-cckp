3a6dda7ad7891ce812ee92987a170897c3e9d18b
Fabrice Brito <fabrice.brito@terradue.com>
2014-06-07 07:41:26 +0200

    Create CreateGeoServerWorkspace.R

---
+++ src/main/R/ReoWBcckp/R/CreateGeoServerWorkspace.R
@@ -0,0 +1,20 @@
+#' creates a workspace in a GeoServer instance
+#' @description Creates a GeoServer workspace in a GeoServer instance. 
+#'
+#' @param geoserver.access.point GeoServer REST API access point
+#' @param workspace workspace name to be created
+#' @return boolean TRUE when the workspace was created, FALSE otherwise (check message returned)
+#' @examples \dontrun{
+#' CreateGeoServerWorkspace("PRT")
+#' }
+#'
+#' @export
+#' @import RCurl
+
+CreateGeoServerWorkspace <- function(geoserver.access.point, workspace) {
+
+  content <- 
+
+  server.response <- httpPUT(url=geoserver.access.point, content=content)
+
+}
