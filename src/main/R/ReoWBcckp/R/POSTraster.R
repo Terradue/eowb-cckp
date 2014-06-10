26e2855ec0baad8027eff882147b0cd4a3e6b18b
Fabrice Brito <fabrice.brito@terradue.com>
2014-06-10 15:03:52 +0200

    Update POSTraster.R

--- src/main/R/ReoWBcckp/R/POSTraster.R
+++ src/main/R/ReoWBcckp/R/POSTraster.R
@@ -1,4 +1,22 @@
+#' Posts a raster to a GeoServer 
+#' @description Posts a raster to a GeoServer given an existing workspace and coverage store. 
+#'
+#' @param access.point GeoServer REST API access point
+#' @param workspace GeoServer workspace 
+#' @param coverage.store GeoServer coverage store
+#' @param raster raster
+#' @return charecter vector
+#' @examples \dontrun{
+#' filename <- "/Users//fbrito/Downloads/ARMOR3D_REPv3-1_20121121_20140420-2.nc"
+#' r <- raster(filename)
+#' POSTraster("http://localhost:8080/geoserver/rest", workspace="aname", coverage.store="astore", r)
+#' }
+#'
+#' @export
+#' @import raster
+
 POSTraster <- function(access.point, workspace, coverage.store, raster) {
+  # todo: use RCurl instead of the system call
   
   # store the current work dir
   orig.wd <- getwd()
