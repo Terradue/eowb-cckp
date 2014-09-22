fe83dfcf1247f3e02ebbfe24ea36d562ebece116
Costanzo <costanzo.d@gmail.com>
2014-09-22 14:47:13 +0200

    countries with no EEZ
    managing countries with no EEZ

--- src/main/R/ReoWBcckp/R/GetCountryEEZ.R
+++ src/main/R/ReoWBcckp/R/GetCountryEEZ.R
@@ -1,8 +1,8 @@
 #'provides the country EEZ spatial polygon(s)
 #'@description Checks if the country code entered is valid 
-#' and provides the country exclusive economic zone (EEZ) spatial polygon(s) 
+#' and provides the country exclusive economic zone (EEZ) spatial polygon(s). If the country doesn't have an EEZ associated, return NA
 #'
-#'@param ISO.code The 3 letter country code based on ISO3 Country abbreviations (http://unstats.un.org/unsd/methods/m49/m49alpha.htm)
+#'@param ISO.Code The 3 letter country code based on ISO3 Country abbreviations (http://unstats.un.org/unsd/methods/m49/m49alpha.htm)
 #'@return SpatialPolygons
 #'@examples \dontrun{
 #'GetCountryEEZ("PRT")
@@ -14,7 +14,7 @@
 GetCountryEEZ <- function(ISO.Code) {
 
   if (IsISOCodeInvalid(ISO.Code)) { stop(paste(ISO.Code, "is not valid", sep=" ")) }
-  
-  return(SpatialPolygons(world_EEZ_V8_2014[world_EEZ_V8_2014$ISO_3digit == toupper(ISO.Code),]@polygons))
+  result <- try(SpatialPolygons(world_EEZ_V8_2014[world_EEZ_V8_2014$ISO_3digit == toupper(ISO.Code),]@polygons), silent = TRUE)
+  if (class(result) == "try-error") return(NA) else return(result)
   
 }
--- src/main/R/ReoWBcckp/R/GetCountryEnvelope.R
+++ src/main/R/ReoWBcckp/R/GetCountryEnvelope.R
@@ -2,8 +2,8 @@
 #'@description Checks if the country code entered is valid 
 #' and provides the geographical envelope of the country exclusive economic zone (EEZ) 
 #'
-#'@param ISO.code The 3 letter country code based on ISO3 Country abbreviations (http://unstats.un.org/unsd/methods/m49/m49alpha.htm)
-#'@return character representation of the geographical envelope as xmin, ymin, xmax, ymax
+#'@param ISO.Code The 3 letter country code based on ISO3 Country abbreviations (http://unstats.un.org/unsd/methods/m49/m49alpha.htm)
+#'@return character representation of the geographical envelope as xmin, ymin, xmax, ymax. Return NA if no bounding box found for the ISO.Code passed
 #'@examples \dontrun{
 #'GetCountryEnvelope("PRT")
 #'}
@@ -14,9 +14,13 @@
 GetCountryEnvelope <- function(ISO.Code) {
 
   if (IsISOCodeInvalid(ISO.Code)) { stop(paste(ISO.Code, "is not valid", sep=" ")) }
-  
-  bbox <- gEnvelope(world_EEZ_V8_2014[world_EEZ_V8_2014$ISO_3digit == toupper(ISO.Code),])@bbox
-
-  print(paste(bbox[1,1], bbox[2,1], bbox[1,2], bbox[2,2], sep=","))
-  
+  temp <- try(world_EEZ_V8_2014[world_EEZ_V8_2014$ISO_3digit == toupper(ISO.Code),], silent = TRUE)
+  if (class(temp) == "try-error") {
+       bbox <-NA 
+       print(paste("No bounding box associated to", ISO.Code))      
+  }else{
+       bbox <- gEnvelope(temp)@bbox
+       print(paste(bbox[1,1], bbox[2,1], bbox[1,2], bbox[2,2], sep=","))     
+  }
+  return(bbox) 
 }
